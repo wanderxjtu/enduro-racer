65f8dc8d3a284b7b52ba0542a84bab01b928ea93
wanderxjtu <wanderhuang@gmail.com>
2019-02-09 10:33:53 +0800

    add django-allauth

--- enduro_racer/enduro_racer/urls.py
+++ enduro_racer/enduro_racer/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import race
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('race/', race)
 ]
