42368e17d8becd94431d155c9e423fa112b5e241
wanderxjtu <wanderhuang@gmail.com>
2019-03-14 15:48:31 +0800

    style updte

--- enduro_racer/result/views.py
+++ enduro_racer/result/views.py
@@ -1,27 +1,35 @@
 from collections import defaultdict
+from functools import partial
 
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import TemplateView
 
+RACE_NAME = {
+    "sy0324": "2019中国绍兴上虞祝家庄国际单车速降赛"
+}
+
 
 class ResultView(TemplateView):
     template_name = "result.html"
 
     def get_context_data(self, **kwargs):
-        return {"compname": self.kwargs['competition_uniname'],
-                "result": self._read_result(self.kwargs['competition_uniname'])}
+        compname = self.kwargs['competition_uniname']
+        return {"compname": RACE_NAME.get(compname, "比赛"),
+                "cert_prefix": "/cert/{}".format(compname),
+                "result": self._read_result(compname)}
 
     def _read_result(self, name):
         filepath = "/home/admin/%s/result.csv"
-        keys = ["rank", "no", "name", "team", "start", "end", "result", "diff", "points"]
+        keys = ["rank", "no", "name", "team", "start", "end", "certfilename", "result", "diff", "points"]
         result = defaultdict(list)
         with open(filepath) as f:
             for line in f:
                 values = line.split(",")
                 group = values[0].strip()
-                result[group].append(dict(zip(keys, map(str.strip, values[1:]))))
+                result[group].append(dict(zip(keys, map(lambda s: s.strip("-"),
+                                                        map(str.strip, values[1:])))))
 
         for riderlist in result.values():
             riderlist.sort(key=lambda x: x["no"])
