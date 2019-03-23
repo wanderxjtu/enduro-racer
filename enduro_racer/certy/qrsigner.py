01ad93d1bc3cc3183d4729ba50d361696f3d816a
wanderxjtu <wanderhuang@gmail.com>
2019-03-23 19:39:44 +0800

    add verify function

--- enduro_racer/certy/qrsigner.py
+++ enduro_racer/certy/qrsigner.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import tempfile
+
 import qrcode
 import subprocess
 import base64
@@ -11,3 +13,14 @@ def qrsign(privkey_file, content):
     cproc = subprocess.run(cmd, input=content.encode('utf-8'), check=True, stdout=subprocess.PIPE)
 
     return qrcode.make(content.encode('utf-8') + b"|" + base64.b64encode(cproc.stdout), image_factory=WandImage)
+
+
+def verify(privkey_file, content_sig):
+    content, sig = content_sig.split("|", maxsplit=1)
+    signature = base64.b64decode(sig.encode(), validate=True)
+    with tempfile.NamedTemporaryFile(prefix="ere") as sigfile:
+        sigfile.write(signature)
+        sigfile.flush()
+        cmd = ["openssl", "dgst", "-sha256", "-verify", privkey_file, "-signature", sigfile.name]
+        cproc = subprocess.run(cmd, input=content.encode('utf-8'), check=True, stdout=subprocess.PIPE)
+        return b"OK" in cproc.stdout
