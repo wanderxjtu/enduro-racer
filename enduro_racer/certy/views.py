f6d9b889e48f2570ad21cb6eadb6c6761419301c
wanderxjtu <wanderhuang@gmail.com>
2019-03-23 19:44:42 +0800

    add verify/verify_result page

--- enduro_racer/certy/views.py
+++ enduro_racer/certy/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.forms import Form, CharField
 
 # Create your views here.
+from django.views.generic import TemplateView, FormView
+import requests
+
+from certy.qrsigner import verify
+from race.utils import get_client_ip
+
+
+class VerifyForm(Form):
+    message = CharField()
+    vaptch_token = CharField()
+
+    def vaptcha_validate(self, token, request):
+        try:
+            data = {
+                "id": "",
+                "secret_key": settings.VAPTCHA_SECRET,
+                "scene": "03",
+                "token": token,
+                "ip": get_client_ip(request)
+            }
+            req = requests.post("http://api.vaptcha.com/v2/validate", data=data)
+            ret = req.json()
+            return ret["success"] == 1
+        except Exception:
+            return False
+
+
+class VerifyView(TemplateView):
+    template_name = "verify.html"
+
+
+class VerifyResultView(FormView):
+    template_name = "verify_result.html"
+    form_class = VerifyForm
+
+    def form_valid(self, form):
+        if not form.vaptcha_validate(form.vaptcha_token, self.request):
+            return self.render_to_response({"result": "Unknown error."})
+        if verify(settings.CERT_PUBKEY_PATH, form.cleaned_data['message']):
+            return self.render_to_response({"result": "Verify Success!"})
+        return self.render_to_response({"result": "Verify Failed!"})
+
+    def form_invalid(self, form):
+        return self.render_to_response({"result": "Please check your input."})
