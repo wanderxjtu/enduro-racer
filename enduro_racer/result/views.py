154f70a8e762e7550fed38ece1e14fe8fbe2f9a5
wanderxjtu <wanderhuang@gmail.com>
2019-04-03 20:18:46 +0800

    remove race_name map

--- enduro_racer/result/views.py
+++ enduro_racer/result/views.py
@@ -4,12 +4,6 @@ from django.views.generic import TemplateView
 
 from race.models.competition import Competition
 
-RACE_NAME = {
-    "sy2019s1": "2019绍兴·上虞丰惠祝家庄国际单车山地速降邀请赛",
-    "nh2019s1": "2019宁海Enduro春季赛",
-    "cat2019s1": "2019西湖小野猫Enduro赛",
-}
-
 
 class ResultView(TemplateView):
     template_name = "result.html"
@@ -19,7 +13,7 @@ class ResultView(TemplateView):
         try:
             comp_full_name = Competition.objects.values_list("name", flat=True).get(uniname=compname)
         except Exception as e:
-            comp_full_name = RACE_NAME.get(compname, "比赛")
+            comp_full_name = "比赛"
 
         return {"comp_full_name": comp_full_name,
                 "cert_prefix": "/certs/{}".format(compname),
