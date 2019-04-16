5549f1802d48967e5115b578bd0d491dc706aa3b
wanderxjtu <wanderhuang@gmail.com>
2019-04-16 18:56:13 +0800

    result:support multiple game under one competition event

--- enduro_racer/result/views.py
+++ enduro_racer/result/views.py
@@ -14,18 +14,19 @@ class ResultView(TemplateView):
 
     def get_context_data(self, **kwargs):
         compname = self.kwargs['competition_uniname']
+        game = self.kwargs.get('game', None)
         try:
             comp_full_name = Competition.objects.values_list("name", flat=True).get(uniname=compname)
         except Exception as e:
             comp_full_name = "比赛"
 
-        headers, result = self._read_result(compname)
+        headers, result = self._read_result(compname, game)
 
         return {"comp_full_name": comp_full_name,
                 "result": result,
                 "headers": headers}
 
-    def _read_result(self, name):
+    def _read_result(self, name, game=None):
         def _dict_formatter(d, formats):
             cert_link = "/certs/{}/{}".format(name, d["certfilename"]) if d["certfilename"] else ""
             return map(lambda x: x.format(**d, cert_link=cert_link), formats)
@@ -38,7 +39,8 @@ class ResultView(TemplateView):
         headers = zip(cls, conf["th"])
 
         result = defaultdict(list)
-        filepath = os.path.join(basedir, "result.csv")
+        filename = game or "result"
+        filepath = os.path.join(basedir, filename + ".csv")
         with open(filepath) as f:
             tdformat = conf["td"]
             for line in f:
