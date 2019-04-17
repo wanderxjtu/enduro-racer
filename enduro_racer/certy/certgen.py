add304203bc3857ab3a5cb25ffa1e0f2fa0e5fdb
wanderxjtu <wanderhuang@gmail.com>
2019-04-17 11:22:34 +0800

    refactor certgen to a class based

--- enduro_racer/certy/certgen.py
+++ enduro_racer/certy/certgen.py
@@ -13,40 +13,47 @@ from certy.qrsigner import qrsign
 from certy.competition_config import TEMPLATE_CONFIG
 
 
-def get_draw():
-    draw = Drawing()
-    draw.font = settings.CERT_FONT_PATH
-    draw.text_antialias = True
-    return draw
+class CertGen(object):
+    def __init__(self, compname):
+        self.comp_name = compname
+        self.temp_conf = TEMPLATE_CONFIG[compname]
 
+    def get_draw(self):
+        draw = Drawing()
+        draw.font = settings.CERT_FONT_PATH
+        draw.text_antialias = True
+        return draw
 
-def render_cert(temp_name, *contents):
-    """contents should be ordered as 'name, rank, cate, result, ...' """
-    temp_conf = TEMPLATE_CONFIG[temp_name]
-    temp = temp_conf.template
-    image = temp.get_image()
-    for content, style in zip(contents + temp_conf.extra_contents, temp.content_styles):
-        draw = get_draw()
-        draw.font_size = style.font_size
-        draw.fill_color = Color(style.font_color)
-        draw.text_alignment = style.text_align
-        if style.formatter:
-            content = style.formatter(content)
-        draw.text(*style.position, content)
-        draw(image)
-        del draw
+    def get_cert_filename(self, rank, name):
+        fn = rank + name + self.temp_conf.template.file_suffix
+        return fn.replace("/", "-")
 
-    msg = "".join(contents) + temp_conf.name
-    qrimage = qrsign(settings.CERT_KEY_PATH, msg,
-                     fill_color=temp.qr_style.fill_color, back_color=temp.qr_style.back_color)
-    qrimage.resize(*temp.qr_style.size)
+    def render_cert(self, *contents, filename=None):
+        """contents should be ordered as 'name, rank, cate, result, ...' """
+        temp = self.temp_conf.template
+        image = temp.get_image()
+        for content, style in zip(contents + self.temp_conf.extra_contents, temp.content_styles):
+            draw = self.get_draw()
+            draw.font_size = style.font_size
+            draw.fill_color = Color(style.font_color)
+            draw.text_alignment = style.text_align
+            if style.formatter:
+                content = style.formatter(content)
+            draw.text(*style.position, content)
+            draw(image)
+            del draw
 
-    image.composite(qrimage, *temp.qr_style.position)
+        msg = "".join(contents) + self.temp_conf.name
+        qrimage = qrsign(settings.CERT_KEY_PATH, msg,
+                         fill_color=temp.qr_style.fill_color, back_color=temp.qr_style.back_color)
+        qrimage.resize(*temp.qr_style.size)
 
-    filename = "-".join(contents[1::-1]) + temp.file_suffix
+        image.composite(qrimage, *temp.qr_style.position)
 
-    image.save(filename=os.path.join(settings.CERT_SAVE_PATH, temp_name, filename.replace("/", "-")))
+        image.save(filename=os.path.join(settings.CERT_SAVE_PATH, self.comp_name,
+                                         filename or self.get_cert_filename(*contents[1::-1])))
 
 
 if __name__ == "__main__":
-    render_cert(*sys.argv[1:6])
+    g = CertGen(sys.argv[1])
+    g.render_cert(*sys.argv[1:6])
