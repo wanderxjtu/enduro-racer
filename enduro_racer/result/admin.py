ec80e52cad476823634da94ddc016ef00ee9f2b1
wanderxjtu <wanderhuang@gmail.com>
2019-09-30 14:38:42 +0800

    overide resultsmeta/results add_view for loading results from filesystem

--- enduro_racer/result/admin.py
+++ enduro_racer/result/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from race.models import Config
 from result.models import RacerResults, ResultsMeta
+from result.read_result_raw_bb import BBRawResultReader
+
+
+def load_result_view(self, request, form_url='', extra_context=None):
+    try:
+        ongoing_uniname = Config.objects.get(key="LoadingCompetition").value
+    except:
+        ongoing_uniname = None
+    if not ongoing_uniname:
+        self.message_user(request, "加载记录失败，当前没有进行中的比赛", 30)
+    else:
+        BBRawResultReader().read_result(ongoing_uniname)
+        self.message_user(request, "加载了 %s 比赛记录" % ongoing_uniname)
+    return HttpResponseRedirect("../")
 
 
 class ResultsAdmin(admin.ModelAdmin):
@@ -18,10 +36,12 @@ class ResultsAdmin(admin.ModelAdmin):
     def get_realResult_str(self, obj):
         return obj.realResult.strftime("%H:%M:%S.%f")[:-3]
 
+    add_view = load_result_view
+
 
 class ResultMetaAdmin(admin.ModelAdmin):
     list_display = ("resultId", "belongs_to_comp", "displayname", "valid")
-    list_filter = ("competition__uniname", )
+    list_filter = ("competition__uniname",)
     readonly_fields = ("resultId",)
     actions = ("make_valid",)  # "make_invalid")
 
@@ -37,6 +57,8 @@ class ResultMetaAdmin(admin.ModelAdmin):
         for obj in queryset:
             obj.valid = False
             obj.save()
+
+    add_view = load_result_view
 
     make_valid.short_description = "设置为有效成绩单"
     make_invalid.short_description = "设置为无效成绩单"
